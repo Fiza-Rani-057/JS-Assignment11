88e5a92e1e0949c75364709c239c64461ca6b8c1
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-28 01:03:18 +0500

    Maths Methods

---
+++ script.js
@@ -0,0 +1,17 @@
+//  Question 1
+
+ document.write( "<h1>" + "Question 1" + "<br>"  + "</h1>");
+
+  var number = parseFloat(prompt("Enter a positive number"));
+  var roundoffNumber = Math.round(number);
+  var ceilValue = Math.ceil(number);
+  var floorValue = Math.floor(number);
+
+  document.write("Number:" + number + "<br>");
+  document.write("Round off Value:" + roundoffNumber + "<br>");
+  document.write("Ceil Value is:" + ceilValue + "<br>");
+  document.write("Floor Value is:" + floorValue + "<br>");
+
+//  Question 2
+
+  document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
